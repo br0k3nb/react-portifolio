b60ccbdaffdb6b4389c325b5c5210682cea9b077
Rodrigo <brokenb1312@hotmail.com>
2023-04-27 11:31:17 -0300

    fixing cards size on mobile devices

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -29,16 +29,16 @@ export default function Projects() {
               return (
                   <motion.div
                     initial={ project.underConstruction ? { opacity: 0.80 } : { opacity: 1 }}
-                    whileHover={{ scale: 1.05, opacity: 1 }}
+                    whileHover={{ scale: 1.04, opacity: 1 }}
                     transition={{ type: "spring", stiffness: 90, damping: 10 }}
-                    className={`w-full xxs:!w-[85%] sm:w-[90%] md:w-1/2 xl:w-[32%] p-7 xxs:py-7 xxs:px-0 xxs:mx-auto`}
+                    className={`w-full xxs:!w-[80%] sm:w-[90%] md:w-1/2 xl:w-[32%] p-7 xxs:py-7 xxs:px-0 xxs:mx-auto`}
                   >
                       <div className={`block shadow-2xl shadow-teal-900/30 hover:shadow-teal-800/100 bg-stone-800 hover:shadow-xl rounded-2xl overflow-hidden `}>
-                        <div className="relative pb-64 overflow-hidden">
+                        <div className="relative pb-64 overflow-hidden xxs:pb-44">
                           {project.underConstruction ? (
                             <div className="absolute inset-0">
                               <div className="relative flex items-center">
-                                <div className='z-10 absolute w-full bg-teal-600/90 flex flex-row justify-evenly xxs:justify-center rounded-sm shadow-2xl shadow-slate-900 top-24'>
+                                <div className='z-10 absolute w-full bg-teal-600/90 flex flex-row justify-evenly xxs:justify-center rounded-sm shadow-2xl shadow-slate-900 top-24 xxs:top-12'>
                                   <p className="text-2xl py-5 xxs:py-5 xxs:mt-2 xxs:text-sm xxs:uppercase sm:text-lg sm:py-4 sm:uppercase lg:tracking-wide text-center">
                                       Under Construction
                                   </p>
@@ -53,8 +53,8 @@ export default function Projects() {
                         </div>
                         <div className="p-4">
                           <div className="px-2">
-                            <h2 className="mt-2 mb-4 font-thin text-3xl">{project.name}</h2>
-                            <p className="text-base text-gray-300/90">
+                            <h2 className="mt-2 mb-4 font-thin text-3xl xxs:text-2xl">{project.name}</h2>
+                            <p className="text-base text-gray-300/90 xxs:text-sm">
                               {project.description}
                             </p>
                           </div>
@@ -73,8 +73,7 @@ export default function Projects() {
                                 className={`${project.link === '' && 'text-gray-100/70'}`}
                               >
                                 <BsGithub
-                                  size={30}
-                                  className={`hover:-translate-y-1 transition-transform cursor-pointer ${project.link === '' && 'cursor-not-allowed hover:translate-y-0'}`}
+                                  className={`xxs:text-2xl text-3xl hover:-translate-y-1 transition-transform cursor-pointer ${project.link === '' && 'cursor-not-allowed hover:translate-y-0'}`}
                                 />
                               </a>
                               <a 
@@ -83,8 +82,8 @@ export default function Projects() {
                                 className={`${project.link === '' && 'text-gray-100/70'}`}
                               >
                                 <BsArrowUpRightSquare
-                                  size={30}
-                                  className={`hover:-translate-y-1 transition-transform cursor-pointer ${project.link === '' && 'cursor-not-allowed hover:translate-y-0'}`}
+                                  // size={30}
+                                  className={`xxs:text-2xl text-3xl hover:-translate-y-1 transition-transform cursor-pointer ${project.link === '' && 'cursor-not-allowed hover:translate-y-0'}`}
                                 />
                               </a>
                             </div> 
