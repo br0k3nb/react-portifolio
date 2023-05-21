d2332a4412ee6da6e1c55c05090ca9d8dc975da8
Rodrigo <brokenb1312@hotmail.com>
2023-05-21 16:47:46 -0300

    adding a image carousel on the projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,54 @@
-import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
-import { motion } from "framer-motion";
+import { useState } from "react";
+import { BsGithub, BsArrowUpRightSquare, BsArrowRight, BsArrowLeft } from "react-icons/bs";
+import { AnimatePresence, motion } from "framer-motion";
 
 import projects from "../datasets/projects";
 
 export default function Projects() {
+  const [ imageToShow, setImageToShow ] = useState(0);
+  const [ direction, setDirection ] = useState(0);
+  const [ deviceScreenSize, setDeviceScreeSize ] = useState(window.innerWidth);
+
+  addEventListener("resize", () => setTimeout(() => setDeviceScreeSize(window.innerWidth), 500));
+
+  const variants = {
+    initial: (direction: number) => {
+      return { x: direction > 0 ? 1000 : -1000, opacity: 0 }
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        x: { type: 'spring', stiffness: 300, damping: 30 },
+        opacity: { duration: 0.2 },
+      },
+    },
+    exit: (direction: number) => {
+      return {
+        x: direction > 0 ? -1000 : 1000,
+        opacity: 0,
+        transition: {
+          x: { type: 'spring', stiffness: 300, damping: 30 },
+          opacity: { duration: 0.2 },
+        },
+      }
+    },
+  }
+
+  const handleLeftClick = (image: any) => {
+    if (imageToShow) setImageToShow(imageToShow - 1);
+    else setImageToShow(image.length - 1);
+
+    setDirection(-1);
+  };
+
+  const handleRightClick = (image: any) => {
+    if(imageToShow < image.length - 1) setImageToShow(imageToShow + 1);
+    else setImageToShow(0);
+
+    setDirection(1);
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -15,7 +60,6 @@ export default function Projects() {
         Projects
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
       </h1>
-
         <motion.div
           initial={{ x: -100, opacity: 0 }}
           whileInView={{ x: 0, opacity: 1 }}
@@ -24,63 +68,90 @@ export default function Projects() {
         >
           <div className="flex justify-center flex-wrap xxs:flex-col">
             {projects.map(project => {
+              const { image, underConstruction, description, name, codeBase, github, link } = project;
+
               return (
-                  <motion.div
-                    initial={ project.underConstruction ? { opacity: 0.80 } : { opacity: 1 }}
-                    whileHover={{ scale: 1.04, opacity: 1 }}
-                    transition={{ type: "spring", stiffness: 90, damping: 10 }}
-                    className={`xxs:!w-[78%] xxs:max-w-[400px] sm:w-[440px] md:w-[470px] lg:w-1/2 xl:w-[490px] p-7 xxs:py-7 xxs:px-0 xxs:mx-auto`}
-                  >
-                      <div className={`block shadow-2xl shadow-teal-900/30 hover:shadow-teal-800/100 bg-stone-800 hover:shadow-xl rounded-2xl overflow-hidden `}>
-                        <div className="relative pb-64 overflow-hidden xxs:pb-44">
-                          {project.underConstruction ? (
-                            <div className="absolute inset-0">
-                              <div className="relative flex items-center">
-                                <div className='z-10 absolute w-full bg-teal-600/90 flex flex-row justify-evenly xxs:justify-center rounded-sm shadow-2xl shadow-slate-900 top-24 xxs:top-12'>
-                                  <p className="text-2xl py-5 xxs:py-5 xxs:mt-2 xxs:text-sm xxs:uppercase sm:text-lg sm:py-4 sm:uppercase lg:tracking-wide text-center">
-                                      Under Construction
-                                  </p>
-                                  <span className="text-6xl xxs:text-4xl xxs:py-4 sm:text-5xl sm:mt-2">👷‍♂️</span>
-                                </div>
-                                <img className="object-cover blur-xs z-0" src={project.image} alt="" />
+                <motion.div
+                  initial={underConstruction ? { opacity: 0.80 } : { opacity: 1 }}
+                  whileHover={deviceScreenSize > 640 ? { scale: 1.04, opacity: 1 } : undefined}
+                  transition={{ type: "spring", stiffness: 80, damping: 10 }} 
+                  className="xxs:!w-[78%] xxs:max-w-[400px] sm:w-[440px] md:w-[470px] lg:w-1/2 xl:w-[490px] p-7 xxs:py-7 xxs:px-0 xxs:mx-auto"
+                >
+                    <div className="block shadow-2xl shadow-teal-900/50 bg-stone-800 rounded-2xl overflow-hidden transition-all duration-300 ease-in-out border border-gray-600 hover:border-gray-500 xxs:hover:border-gray-600">
+                      <div className="relative pb-64 overflow-hidden xxs:pb-44">
+                        {underConstruction ? (
+                          <div className="absolute inset-0">
+                            <div className="relative flex items-center">
+                              <div className='z-10 absolute w-full bg-teal-600/90 flex flex-row justify-evenly xxs:justify-center rounded-sm shadow-2xl shadow-slate-900 top-24 xxs:top-12'>
+                                <p className="text-2xl py-5 xxs:py-5 xxs:mt-2 xxs:text-sm xxs:uppercase sm:text-lg sm:py-4 sm:uppercase lg:tracking-wide text-center">
+                                    Under Construction
+                                </p>
+                                <span className="text-6xl xxs:text-4xl xxs:py-4 sm:text-5xl sm:mt-2">👷‍♂️</span>
                               </div>
+                              <img className="object-cover blur-xs z-0" src={project.image[0]} alt="Project image" />
                             </div>
-                          ): (
-                            <img className="absolute inset-0 h-full w-full object-cover" src={project.image} alt="Project image" />
-                          )}
-                        </div>
-                        <div className="p-4">
-                          <div className="px-2">
-                            <h2 className="mt-2 mb-4 font-thin text-3xl xxs:text-2xl">{project.name}</h2>
-                            <p className="text-base text-gray-300/90 xxs:text-sm">
-                              {project.description}
-                            </p>
                           </div>
-                        </div>
-                        <div className="p-4">
-                          <div className="px-2">
-                            <div className="mb-8 flex flex-row space-x-2">
-                              {project.codeBase?.map((icons, idx) => <img draggable={false} src={icons} alt="" key={idx} className='h-[30px] w-[30px] xxs:w-[30px] xxs:h-[35px]'/> )}
-                            </div>
-                            <div className="flex flex-row align-bottom space-x-4 mb-2">
-                              <a 
-                                draggable={false}
-                                href={project.github === '' ? '#' : project.github}
-                                className={`${project.link === '' && 'text-gray-100/70'}`}
-                              >
-                                <BsGithub className={`xxs:text-2xl text-3xl hover:-translate-y-1 transition-transform cursor-pointer ${project.link === '' && 'cursor-not-allowed hover:translate-y-0'}`}/>
-                              </a>
-                              <a 
-                                draggable={false}
-                                href={project.link === '' ? '#' : project.link}
-                                className={`${project.link === '' && 'text-gray-100/70'}`}
-                              >
-                                <BsArrowUpRightSquare className={`xxs:text-2xl text-3xl hover:-translate-y-1 transition-transform cursor-pointer ${project.link === '' && 'cursor-not-allowed hover:translate-y-0'}`}/>
-                              </a>
-                            </div> 
-                          </div>
+                        ) : (
+                          <>
+                            <BsArrowRight 
+                              onClick={() => handleRightClick(image)} 
+                              size={35} 
+                              className="z-10 absolute top-[7.4rem] xxs:top-[5rem] right-1 cursor-pointer bg-gray-900 px-2 py-1 rounded-full"
+                            />
+                            <BsArrowLeft
+                              onClick={() => handleLeftClick(image)}
+                              size={35} 
+                              className="z-10 absolute top-[7.4rem] xxs:top-[5rem] active:ring-0 left-1 cursor-pointer bg-gray-900 px-2 py-1 rounded-full"
+                            />
+                            
+                            <AnimatePresence initial={false} custom={direction}>
+                              <motion.img
+                                exit="exit"
+                                animate="animate"
+                                initial="initial"
+                                custom={direction}
+                                variants={variants}
+                                alt="Project images"
+                                src={image[imageToShow]}
+                                key={image[imageToShow]}
+                                className="absolute inset-0 h-full w-full object-cover"
+                              />
+                            </AnimatePresence>
+                          </>
+                        )}
+                      </div>
+                      <div className="p-4">
+                        <div className="px-2">
+                          <h2 className="mt-2 mb-4 font-thin text-3xl xxs:text-2xl">{name}</h2>
+                          <p className="text-base text-gray-300/90 xxs:text-sm">
+                            {description}
+                          </p>
                         </div>
                       </div>
+                      <div className="p-4">
+                        <div className="px-2">
+                          <div className="mb-8 flex flex-row space-x-2">
+                            {codeBase?.map((icons, idx) => <img draggable={false} src={icons} alt="" key={idx} className='h-[30px] w-[30px] xxs:w-[30px] xxs:h-[35px]'/> )}
+                          </div>
+                          <div className="flex flex-row align-bottom space-x-4 mb-2">
+                            <a 
+                              draggable={false}
+                              href={github ? github as string : '#'}
+                              className={`${!link && 'text-gray-100/70'}`}
+                            >
+                              <BsGithub className={`xxs:text-2xl text-3xl hover:-translate-y-1 transition-transform cursor-pointer ${!link && 'cursor-not-allowed hover:translate-y-0'}`}/>
+                            </a>
+                            <a 
+                              draggable={false}
+                              href={link ? link as string : '#'}
+                              className={`${!link && 'text-gray-100/70'}`}
+                            >
+                              <BsArrowUpRightSquare className={`xxs:text-2xl text-3xl hover:-translate-y-1 transition-transform cursor-pointer ${!link && 'cursor-not-allowed hover:translate-y-0'}`}/>
+                            </a>
+                          </div> 
+                        </div>
+                      </div>
+                    </div>
                   </motion.div>
                 )
               })}
--- src/datasets/projects.ts
+++ src/datasets/projects.ts
@@ -1,16 +1,21 @@
-import mobile from '../assets/mobile.jpg';
-import notes from '../assets/noap_sign_in.png';
 import csgo from '../assets/csgo.jpg';
-import financial from '../assets/financial.png';
+import mobile from '../assets/mobile.jpg';
 import builder from '../assets/builder.png';
 import shopper from '../assets/shopper.png';
+import notes from '../assets/noap_sign_in.png';
+import financial from '../assets/financial.png';
+import noap1 from '../assets/noap-screenshots1.png';
+import noap2 from '../assets/noap-screenshots2.png';
+import noap3 from '../assets/noap-screenshots3.png';
+import noap4 from '../assets/noap-screenshots4.png';
 import shopperMobile from '../assets/shopper-mobile.png';
+
 
 export default [
   {
     "name": "Noap",
     "description": "Noap is a notes application heavily inspired by the Evernote app. It's being built using the MERN stack, (with typescript), Lexical editor and a lot of different packages, like: React Query, React Hook Form and Framer Motion. For more information about the tech behind Noap, please have a look at the repository!",
-    "image": notes,
+    "image": [ notes, noap1, noap2, noap3, noap4 ],
     "github": "https://github.com/br0k3nb/Noap-typescript",
     "link": "https://noap.vercel.app/",
     "underConstruction": false,
@@ -25,9 +30,9 @@ export default [
   {
     "name": "BuilderIO",
     "description": "A application that lets you see the prices of the available parts and build your dream pc! 🖥️",
-    "image": builder,
-    "github": "",
-    "link": "",
+    "image": [ builder ],
+    "github": false,
+    "link": false,
     "underConstruction": true,
     "codeBase": [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
@@ -40,9 +45,9 @@ export default [
   {
     "name": "Shopper",
     "description": "A fully responsive and functional e-commerce website 🛒",
-    "image": shopper,
-    "github": "",
-    "link": "",
+    "image": [ shopper ],
+    "github": false,
+    "link": false,
     "underConstruction": true,
     "codeBase": [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
@@ -55,9 +60,9 @@ export default [
   {
     "name": "Shopper Mobile",
     "description": "Mobile version of Shopper built with React Native 🛒📱",
-    "image": shopperMobile,
-    "github": "",
-    "link": "",
+    "image": [ shopperMobile ],
+    "github": false,
+    "link": false,
     "underConstruction": true,
     "codeBase": [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
@@ -70,9 +75,9 @@ export default [
   {
     "name": "Job+",
     "description": "Job+ is a mobile application designed to help you find a fitting job 🏢",
-    "image": mobile,
-    "github": "",
-    "link": "",
+    "image": [ mobile ],
+    "github": false,
+    "link": false,
     "underConstruction": true,
     "codeBase": [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
@@ -85,9 +90,9 @@ export default [
   {
     "name": "Expenser",
     "description": "Expenser is a financial mobile application to help you keep track of your money 💸",
-    "image": financial,
-    "github": "",
-    "link": "",
+    "image": [ financial ],
+    "github": false,
+    "link": false,
     "underConstruction": true,
     "codeBase": [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
@@ -99,9 +104,9 @@ export default [
   {
     "name": "Map picker",
     "description": "A configurable map picker for Counter-Strike: Global Offensive and VALORANT",
-    "image": csgo,
-    "github": "",
-    "link": "",
+    "image": [ csgo ],
+    "github": false,
+    "link": false,
     "underConstruction": true,
     "codeBase": [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
