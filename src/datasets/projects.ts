5cd42e5469accffa18d2038f942f145739fe6393
Rodrigo Oliveira <brokenb1312@hotmail.com>
2023-10-04 17:54:03 -0300

    changing the link of the Noap project

--- src/datasets/projects.ts
+++ src/datasets/projects.ts
@@ -18,19 +18,33 @@ import shopperMobile from '../assets/shopper-mobile.png';
 export default [
   {
     "name": "Noap",
-    "description": "Noap is a notes application heavily inspired by the Evernote app. It's being built using the MERN stack, (with typescript), Lexical editor and a lot of different packages, like: React Query, React Hook Form and Framer Motion. For more information about the tech behind Noap, please have a look at the repository!",
+    "description": "Noap is a notes application heavily inspired Evernote. It's being built using the MERN stack, (with typescript), Lexical editor and a lot of different packages, like: React Query, React Hook Form and Framer Motion. For more information about the tech behind Noap, please have a look at the repository!",
     "image": [noap, noap1, noap2, noap3, noap4, noap5, noap6, noap7, noap8, noap9],
-    "github": "https://github.com/br0k3nb/Noap-typescript",
+    "github": "https://github.com/br0k3nb/noap",
     "link": "https://noap.vercel.app/",
     "underConstruction": false,
     "codeBase": [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
     ]
   },
+  // {
+  //   "name": "Noap (backend)",
+  //   "description": "Noap is a notes application heavily inspired Evernote. It's being built using the MERN stack, (with typescript), Lexical editor and a lot of different packages, like: React Query, React Hook Form and Framer Motion. For more information about the tech behind Noap, please have a look at the repository!",
+  //   "image": [noap, noap1, noap2, noap3, noap4, noap5, noap6, noap7, noap8, noap9],
+  //   "github": "https://github.com/br0k3nb/noap-backend",
+  //   "link": "https://github.com/br0k3nb/noap-backend",
+  //   "underConstruction": false,
+  //   "codeBase": [
+  //     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+  //     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  //     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+  //     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  //   ]
+  // },
   {
     "name": "BuilderIO",
     "description": "A application that lets you see the prices of the available parts and build your dream pc! 🖥️",
